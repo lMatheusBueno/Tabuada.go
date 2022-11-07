a694869e974fab96192d4d199f306623d04f6a0b
Matheus Vinicius Bueno <94638250+lMatheusBueno@users.noreply.github.com>
2022-11-07 16:05:51 -0300

    TabuadaGo

--- Tabuada.go
+++ Tabuada.go
@@ -5,10 +5,11 @@ import "fmt"
 func main() {
 	fmt.Println("****Tabuada em Go!****")
 	for i := 1; i <= 10; i++ { //primeiro laço fará 10 repetições
-		for y := 1; y <= 10; y++ { //segundo laço fará 100 repetições
+		for y := 1; y <= 10; y++ { //segundo laço fará 10 epetições
 
 			fmt.Println(i, " x ", y, " = ", i*y)
 
 		}
+		fmt.Println("\n") //quebra linha 
 	}
 }
