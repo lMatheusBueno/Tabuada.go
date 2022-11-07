2a9e7c908f63829cec73f799f621ac21ffc31799
matheus_jda@hotmail.com <lMatheusBueno>
2022-11-07 15:17:52 -0300

    TabuadaGo

---
+++ Tabuada.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const a = 10
+
+func main() {
+	fmt.Println("****Tabuada em Go!****")
+	for i := 1; i <= 10; i++ { //primeiro laço fará 10 repetições
+		for y := 1; y <= 10; y++ { //segundo laço fará 100 repetições
+
+			fmt.Println(i, " x ", y, " = ", i*y)
+
+		}
+	}
+}
